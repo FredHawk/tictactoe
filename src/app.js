93e8fde841fec4ba06cc59836d82a6b5fe501e4d
eyece <fredrichawk@gmail.com>
2017-03-07 06:41:26 +0100

    Added some initial building blocks for the game

--- src/app.js
+++ src/app.js
@@ -1,4 +1,40 @@
 import css from './style.css';
 
-const gameboard = [];
+const game = {
+  currentPlayer: 'X',
+  human: '',
+  computer: '',
+  turn: 0,
+  board: [null, null, null,
+          null, null, null,
+          null, null, null],
+}
 
+// Function that calculates if someone has won
+function whoWon () {
+
+}
+
+// Function that runs when turn = 9 (whole board is filled and no one won)
+function draw() {
+
+}
+
+// Function that changes who the currentPlayer is
+function changePlayer () {
+  if (game.currentPlayer == 'X') {
+    game.currentPlayer = 'O';
+  } else {
+    game.currentPlayer = 'X';
+  }
+}
+
+// AI function - Don't know how to do this one yet.
+function aiMove () {
+
+}
+
+// Update state ? updating the board and changing player and other stuff.
+function updateState () {
+
+}
